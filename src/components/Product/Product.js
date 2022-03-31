549d0c6f28d4f9ce84b52925d1aeb89670f5d4ac
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-01 02:51:12 +0600

    new code

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart } from 
+'@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import './Product.css'
 
 const Product = (props) => {
-   const {name, img, seller, price, rating} = props.product;
+   const {name, img, seller, price, ratingsCount, category} = props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,7 +14,8 @@ const Product = (props) => {
             <p className='product-name'>{name}</p>
             <p>Price: ${price}</p>
             <p><small>Seller:{seller}</small></p>
-            <p><small>Ratings: {rating} stars</small></p>
+            <p><small>Ratings: {ratingsCount} <FontAwesomeIcon style={{color: 'gold'}} icon={faStar}></FontAwesomeIcon></small></p>
+            <p><small>Category: {category}</small></p>
             </div>
             <button onClick={() => props.addToCart (props.product)} className='btn-cart'>
                 <p className='btn-text'>Add to cart</p>
