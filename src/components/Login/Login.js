7b8847b3160babefc4185488693b3c8732f82fed
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-10 14:01:21 +0600

    new code

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import './Login.css'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
 const Login = () => {
-    return (
-        <div>
-            <h2>This is login</h2>
-        </div>
-    );
+  return (
+    <div className="form-container">
+      <div>
+      <h2 className="from-title">Login</h2>
+         <form>
+         <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="" />
+      </div>
+          <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="Password" id="" />
+          </div>
+          <input className="form-submit" type="submit" value="Login" />
+         </form>
+         <p>
+             New to Ema-John? <Link className="form-link" to='/signup'>Create account</Link>
+         </p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
