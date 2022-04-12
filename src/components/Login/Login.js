ed7e400467499a543a989e2701eaede0ba364644
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-12 19:26:41 +0600

    new cod

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
 import "./Login.css";
+
 const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -12,18 +13,22 @@ loading,
 error,
 ] = useSignInWithEmailAndPassword(auth)
 
+  const navigate = useNavigate()
+  const location = useLocation()
+  const from =location.state?.from.pathname|| '/'
+
   const handleEmailBlur = event =>{
     setEmail(event.target.value)
   }
 
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
 
   const handlePasswordBlur = event =>{
     setPassword(event.target.value)
   }
 
     if(user){
-    navigate('/shop')
+    navigate(from, {replace: true})
     }
 
 
